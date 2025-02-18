08bac3b2f3aa775e123d7c7bd3162a88a3f8fe5f
Julián D. Arias-Londoño <judaarlo@gmail.com>
2025-02-18 18:00:38 -0500

    repo structure

--- data/dataset.py
+++ data/dataset.py
@@ -10,6 +10,7 @@ import numpy as np
 SAMPLE_RATE = 44100
 DATA_PATH_Gita = '/home/jdariasl/Documents/Experiments/Experiments_EmiroIbarra/Parkinson_datasets/Gita/Pataka/'
 DATA_PATH_NeuroV = '/home/jdariasl/Documents/Experiments/Experiments_EmiroIbarra/Parkinson_datasets/Neurovoz/PorMaterial_limpios1_2_downsized/PATAKA/'
+DATA_PATH_SaarB = '/home/jdariasl/Documents/Experiments/Experiments_EmiroIbarra/Saarbruecken_dataset/vowel_a_resampled/'
 
 class Pataka_Dataset(Dataset):
     """
@@ -17,7 +18,7 @@ class Pataka_Dataset(Dataset):
     """
 
     def __init__(self, 
-                 DBs = ['Gita'], #Options are ['Gita','Neurovoz']
+                 DBs = ['Gita'], #Options are ['Gita','Neurovoz', 'Saarbruecken']
                  train_size=0.8,
                  mode = 'train', #Options are ['train','test']
                  seed=1234
@@ -26,41 +27,52 @@ class Pataka_Dataset(Dataset):
         self.train_size = train_size
         self.DBs = DBs
         self.mode = mode
-        self.paths, self.labels, self.speaker_ids = self.read_data()
-        self.signals, self.y_label, self.subject_group = self.process_select_signals(SAMPLE_RATE)
+        self.paths, self.labels, self.speaker_ids, self.dbs_id = self.read_data()
+        self.signals, self.y_label, self.subject_group, self.db_group = self.process_select_signals(SAMPLE_RATE)
         
     def __len__(self):
         return len(self.signals)
     
     def __getitem__(self, index):
         spec = compute_norm_spect(self.signals[index], SAMPLE_RATE)
-        return torch.tensor(spec, dtype=torch.float32), torch.tensor(self.y_label[index], dtype=torch.float32), torch.tensor(self.subject_group[index], dtype=torch.float32)
+        return torch.tensor(spec, dtype=torch.float32), torch.tensor(self.y_label[index], dtype=torch.float32), torch.tensor(self.subject_group[index], dtype=torch.float32), self.db_group[index]
 
 
     def read_data(self):
         paths = []
         labels = []
         speaker_ids = []
+        dbs_id = []
         for DB in self.DBs:
             if DB == 'Gita':
                 paths_Gita, labels_Gita, speaker_ids_Gita = Read_Gita_DB(DATA_PATH_Gita)
                 paths.extend(paths_Gita)
                 labels.extend(labels_Gita)
                 speaker_ids.extend(speaker_ids_Gita)
+                dbs_id.extend([DB]*len(paths_Gita))
             elif DB == 'Neurovoz':
                 Nspeakers = len(speaker_ids)
                 paths_NeuroV, labels_NeuroV, speaker_ids_NeuroV = Read_NeuroVoz_DB(DATA_PATH_NeuroV,Nspeakers)
                 paths.extend(paths_NeuroV)
                 labels.extend(labels_NeuroV)
                 speaker_ids.extend(speaker_ids_NeuroV)
-        return paths, labels, speaker_ids
+                dbs_id.extend([DB]*len(paths_NeuroV))
+            elif DB == 'Saarbruecken':
+                Nspeakers = len(speaker_ids)
+                paths_SaarB, labels_SaarB, speaker_ids_SaarB = Read_Saarbruecken_DB(DATA_PATH_SaarB)
+                paths.extend(paths_SaarB)
+                labels.extend(labels_SaarB)
+                speaker_ids.extend(speaker_ids_SaarB)
+                dbs_id.extend([DB]*len(paths_SaarB))
+        return paths, labels, speaker_ids, dbs_id
 
         #selecting 400ms overlap in 50ms of audio signal example
     def process_select_signals(self, SAMPLE_RATE):
         time_leng=0.4
         sample_leng=int(time_leng*SAMPLE_RATE)
         overloap=2
-        signals, y_label, subject_group, tono =[],[],[],[]
+        signals, y_label, subject_group, db_group =[],[],[],[]
+        signals_sb, y_label_sb, subject_group_sb, db_group_sb =[],[],[],[]
 
 
         #Processs data to train
@@ -78,11 +90,18 @@ class Pataka_Dataset(Dataset):
                     if ind_end <= indx[len(indx)-1]:
                         signal=np.zeros(sample_leng)
                         signal = audio[ind_start:int(ind_end)]
-                        signals.append(signal)
-                
-                        y_label.append(self.labels[data_ind])
-                        subject_group.append(self.speaker_ids[data_ind])
-                        segments=segments+1
+                        if self.dbs_id[data_ind] == 'Saarbruecken':
+                            signals_sb.append(signal)
+                            y_label_sb.append(self.labels[data_ind])
+                            subject_group_sb.append(self.speaker_ids[data_ind])
+                            db_group_sb.append(self.dbs_id[data_ind])
+                            segments=segments+1
+                        else:
+                            signals.append(signal)
+                            y_label.append(self.labels[data_ind])
+                            subject_group.append(self.speaker_ids[data_ind])
+                            db_group.append(self.dbs_id[data_ind])
+                            segments=segments+1
                 print(" Processed {}/{} files".format(self.speaker_ids[data_ind],len(self.paths)-1),end='')
                 print(" Time audio: {} Segments {} ".format((audio_len-1)/sample_rate,segments))
             else:
@@ -91,6 +110,7 @@ class Pataka_Dataset(Dataset):
         signals = np.stack(signals,axis=0)
         y_label = np.stack(y_label,axis=0)
         subject_group = np.stack(subject_group,axis=0)
+        db_group = np.stack(db_group,axis=0)
 
         #Shuffle data
         np.random.seed(self.seed)
@@ -103,7 +123,21 @@ class Pataka_Dataset(Dataset):
         signals = signals[subj_ind==1]
         y_label = y_label[subj_ind==1]
         subject_group = subject_group[subj_ind==1]
-        return signals, y_label, subject_group
+        db_group = db_group[subj_ind==1]
+
+        if len(signals_sb)>0:
+            signals_sb = np.stack(signals_sb,axis=0)
+            y_label_sb = np.stack(y_label_sb,axis=0)
+            subject_group_sb = np.stack(subject_group_sb,axis=0)
+            db_group_sb = np.stack(db_group_sb,axis=0)
+            
+            signals = np.concatenate((signals,signals_sb),axis=0)
+            y_label = np.concatenate((y_label,y_label_sb),axis=0)
+            subject_group = np.concatenate((subject_group,subject_group_sb),axis=0)
+            db_group = np.concatenate((db_group,db_group_sb
+                                       ),axis=0)
+
+        return signals, y_label, subject_group, db_group
      
 def Read_Gita_DB(DATA_PATH_Gita):
     Speaker_PD=0
@@ -149,6 +183,28 @@ def Read_NeuroVoz_DB(DATA_PATH_NeuroVoz,offset=0):
                     Labels.append(Label)
                     Paths.append(file_path)
     print("The number of files in Neurovoz DB is {}".format(len(Paths)))
+    return Paths, Labels, Speaker_IDs
+
+def Read_Saarbruecken_DB(DATA_PATH_SaarB):
+    Speaker_IDs = []
+    Labels = []
+    Paths = []
+    for dirname, _, filenames in os.walk(DATA_PATH_SaarB):
+        for filename in filenames:
+                file_path = os.path.join(dirname, filename)
+                if filename.find('wav')!=-1:
+                    identifiers=filename.split('.')[0].split('_')
+                    Speaker_ID=int(identifiers[0].split('-')[0])
+                    #Tone=identifiers[1]
+                    
+                    if dirname.find('healthy')!=-1:
+                        Label=0
+                    else: 
+                        Label=1
+                    Speaker_IDs.append(Speaker_ID)
+                    Labels.append(Label)
+                    Paths.append(file_path)
+    print("The number of files in Saarbruecken DB is {}".format(len(Paths)))
     return Paths, Labels, Speaker_IDs
 
 def compute_norm_spect(signals, sample_rate):
