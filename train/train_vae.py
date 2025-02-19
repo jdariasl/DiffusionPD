83bef0cb79b2b11d35d73399937ddeb053fc81ce
Julián D. Arias-Londoño <judaarlo@gmail.com>
2025-02-19 16:22:10 -0500

    checkpoint save

--- train/train_vae.py
+++ train/train_vae.py
@@ -6,8 +6,8 @@ import torch.optim as optim
 
 
 
-def train_vae(train_loader, x_dim, z_dim, epochs, lr, device):
-    
+def train_vae(train_loader, test_loader, x_dim, z_dim, epochs, lr, device):
+    best_loss_val = 1e10
     model = VAE(x_dim, z_dim).to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     model.train()
@@ -23,4 +23,27 @@ def train_vae(train_loader, x_dim, z_dim, epochs, lr, device):
             optimizer.step()
         print('====> Epoch: {} Average loss: {:.4f}'.format(
               epoch, train_loss / len(train_loader.dataset)))
+        loss_val = test_vae(test_loader, model, device)
+
+        if loss_val < best_loss_val:
+            best_loss_val = loss_val
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss_val,
+            }, 'saved_models/vae.pth')
+            print('Model saved as vae.pth')
     return model
+
+def test_vae(test_loader, model, device):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, (data, _, _, _) in enumerate(test_loader):
+            data = data.to(device)
+            recon_batch, mu, logvar = model(data)
+            test_loss += vae_loss(recon_batch, data, mu, logvar).item()
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+    return test_loss
