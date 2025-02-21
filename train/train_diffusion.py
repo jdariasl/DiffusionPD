430eec0eacf3c162241017e130cb83decd1b41d7
Julián D. Arias-Londoño <judaarlo@gmail.com>
2025-02-21 11:13:09 -0500

    fix bug

--- train/train_diffusion.py
+++ train/train_diffusion.py
@@ -13,11 +13,11 @@ def train_diffusion(vae, time_steps, train_loader, test_loader, x_dim, z_dim, ep
     
     #set constant diffusion parameters
     diff_params = set_diffusion_params(T = time_steps)
-
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    model.train()
+    
     vae.eval()
     for epoch in range(epochs):
+        model.train()
         train_loss = 0
         for batch_idx, (data, label, _, _) in enumerate(train_loader):
             data = data.to(device)
@@ -34,6 +34,7 @@ def train_diffusion(vae, time_steps, train_loader, test_loader, x_dim, z_dim, ep
         print('====> Epoch: {} Average diff loss: {:.4f}'.format(
               epoch, train_loss / len(train_loader.dataset)))
         
+        test_diffusion(vae, time_steps, test_loader, model, diff_params, device)
     return model
 
 
