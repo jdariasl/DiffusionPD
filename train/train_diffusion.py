cf08199d24c1e6a2e01bd475f40262c67ed0d021
Julian Arias <judaarlo@gmail.com>
2025-05-27 20:50:07 -0500

    fixed bugs in diffusion classifier

--- train/train_diffusion.py
+++ train/train_diffusion.py
@@ -12,6 +12,7 @@ from diffusers.optimization import get_cosine_schedule_with_warmup
 from .sampler_diffusion import Class_DDPMPipeline
 import wandb
 
+
 def train_diffusion(
     vae,
     time_steps,
@@ -26,7 +27,7 @@ def train_diffusion(
     device,
     resume_training=False,
     model=None,
-):  
+):
     # Start a new wandb run to track this script.
     wandb.login(key="d3ab2dfb9825e30136d030033cfd019c6dc63407")
     run = wandb.init(
@@ -45,8 +46,8 @@ def train_diffusion(
             "latent_dim": z_dim,
             "time_steps": time_steps,
             "pred_diff_time": pred_diff_time,
-            },
-        )
+        },
+    )
     # best_loss_val = 1e10
     scaler_diffusion = GradScaler()
     if resume_training:
@@ -56,10 +57,10 @@ def train_diffusion(
         model = UNet(
             in_channels=x_dim, out_channels=1, num_classes=4, init_features=z_dim
         ).to(device)
-    
+
     Norm = BatchNormlizer(num_features=z_dim).to(device)
     Norm.train()
-    
+
     # set constant diffusion parameters
     noise_scheduler = DDPMScheduler(
         num_train_timesteps=time_steps,
@@ -69,8 +70,8 @@ def train_diffusion(
     )
 
     # diff_params = set_diffusion_params(T=time_steps)
-    #params = list(model.parameters()) + list(Norm.parameters())
-    #optimizer = optim.Adam(params, lr=lr)
+    # params = list(model.parameters()) + list(Norm.parameters())
+    # optimizer = optim.Adam(params, lr=lr)
     optimizer = optim.AdamW(model.parameters(), lr=lr)
     lr_scheduler = get_cosine_schedule_with_warmup(
         optimizer=optimizer,
@@ -90,7 +91,7 @@ def train_diffusion(
             with torch.no_grad():
                 mu, _ = vae.encode(data)
                 mu = mu.to(device)
-            #mu = Norm(mu)
+            # mu = Norm(mu)
             optimizer.zero_grad()
             with torch.autocast(device_type="cuda"):
                 t = torch.randint(0, time_steps, (data.shape[0],)).to(device)
@@ -127,19 +128,25 @@ def train_diffusion(
         # scheduler.step()
         # Log metrics to wandb.
         image = sample_plot_image_scheduler(
-                vae,
-                Norm.eval(),
-                model.eval(),
-                time_steps,
-                z_dim,
-                device,
-                "img_samples/",
-                n=1,
-                return_image=True
-            )
+            vae,
+            Norm.eval(),
+            model.eval(),
+            time_steps,
+            z_dim,
+            device,
+            "img_samples/",
+            n=1,
+            return_image=True,
+        )
         images = wandb.Image(image, caption="Top: Output, Bottom: Input")
-         
-        run.log({"train_loss_mse": train_loss / len(train_loader.dataset), "test_loss_class_dist": test_loss, "gen_img": images})
+
+        run.log(
+            {
+                "train_loss_mse": train_loss / len(train_loader.dataset),
+                "test_loss_class_dist": test_loss,
+                "gen_img": images,
+            }
+        )
     run.finish()
     return model, Norm
 
@@ -156,14 +163,22 @@ def test_diffusion_scheduler(
             data = data.to(device)
             mu, _ = vae.encode(data)
             mu = mu.to(device)
-            #mu = Norm(mu)
+
+            noise = torch.randn_like(mu).to(device)
+            mu = noise_scheduler.add_noise(
+                mu,
+                noise,
+                T_pred * torch.ones(mu.shape[0], dtype=torch.int64).to(device),
+            )
+
+            # mu = Norm(mu)
             label = label.long().to(device)
             recover_spec = pipeline(
                 init_samples=mu,
                 class_labels=label,
                 num_inference_steps=T_pred,
                 generator=torch.Generator(device="cpu").manual_seed(1234),
-                device=device
+                device=device,
             )
 
             label_not = (~label.bool()).long().to(device)
@@ -172,7 +187,7 @@ def test_diffusion_scheduler(
                 class_labels=label_not,
                 num_inference_steps=T_pred,
                 generator=torch.Generator(device="cpu").manual_seed(1234),
-                device=device
+                device=device,
             )
 
             loss = torch.mean(
@@ -190,7 +205,7 @@ def test_diffusion(vae, time_steps, test_loader, model, diff_params, device, T_p
     model.eval()
     test_loss = 0
     with torch.no_grad():
-        for i, (data,_, label, _, _) in enumerate(test_loader):
+        for i, (data, _, label, _, _) in enumerate(test_loader):
             data = data.to(device)
             mu, _ = vae.encode(data)
             label = label.long().to(device)
